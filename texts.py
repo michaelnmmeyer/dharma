762248890187e94a852e4edc4ac70235ace21872
Michaël Meyer <michaelnm.meyer@gmail.com>
2023-05-11 22:53:42 +0200

    Filter out repos that don't contain texts

--- texts.py
+++ texts.py
@@ -3,13 +3,13 @@
 import os, re
 from bs4 import BeautifulSoup
 
-def iter_texts():
-	for root, dirs, files in os.walk("repos"):
+def iter_texts_in_repo(name):
+	path = os.path.join("repos", name)
+	for root, dirs, files in os.walk(path):
 		# There are generated files in
 		# repos/tfc-nusantara-epigraphy/workflow-output/editedxml
 		# They are produced with
 		# repos/project-documentation/editorialStylesheets/tpl-editorialConvention.xsl
-
 		# This does some basic formatting (space, quotation marks), but
 		# doesn't change the structure of the document, so we can
 		# ignore it and work on the originals. We'll add something do
@@ -29,6 +29,18 @@ def iter_texts():
 			if file.startswith("DHARMA_mdt"):
 				continue
 			yield os.path.join(root, file)
+
+repos_to_ignore = {
+	"digital-areal",
+	"mdt-authorities",
+	"project-documentation",
+}
+
+def iter_texts():
+	for repo in os.listdir("repos"):
+		if repo in repos_to_ignore:
+			continue
+		yield from iter_texts_in_repo(repo)
 
 for file in iter_texts():
 	print(file)
