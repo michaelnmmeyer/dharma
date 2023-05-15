76246da6e063ce9c3aac1b3092756a11dd7ad8d6
Michaël Meyer <michaelnm.meyer@gmail.com>
2023-05-15 15:53:15 +0200

    Improve the CLI interface of cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -41,7 +41,61 @@ def cleanup_file(f):
 		wrote = True
 		empty = 0
 
+OK = 0
+BINARY = 1
+FAIL = 2
+
+def complain(msg):
+	print("%s: %s" % (os.path.basename(sys.argv[0]), msg), file=sys.stderr)
+
+def normalize_filter(r, w):
+	try:
+		for line in cleanup_file(r):
+			w.write(line)
+			w.flush()
+		return OK
+	except UnicodeDecodeError:
+		return BINARY
+	except Exception as e:
+		complain("%s" % e)
+		return FAIL
+
 if __name__ == "__main__":
-	for line in cleanup_file(sys.stdin):
-		sys.stdout.write(line)
-		sys.stdout.flush()
+	import os, argparse, tempfile, shutil
+	this_dir = os.path.dirname(os.path.abspath(__file__))
+	parser = argparse.ArgumentParser(description="""Normalize text files. Print the result
+		on the standard output, unless the -i option is used, in which case input
+		files are modified in-place.""")
+	parser.add_argument("-i", "--in-place", help="edit files in-place", action="store_true")
+	parser.add_argument("file", nargs="*")
+	args = parser.parse_args()
+	failed = False
+	if args.in_place and args.file:
+		for name in args.file:
+			with tempfile.NamedTemporaryFile(mode="w", dir=this_dir, delete=False) as tmp:
+				try:
+					with open(name) as f:
+						ret = normalize_filter(f, tmp)
+				except FileNotFoundError:
+					complain("'%s': not found" % name)
+					ret = FAIL
+				if ret == OK:
+					shutil.move(tmp.name, name)
+				else:
+					os.remove(tmp.name)
+					if ret == FAIL:
+						failed = True
+	elif args.file:
+		for name in args.file:
+			try:
+				with open(name) as f:
+					if normalize_filter(f, sys.stdout) != OK:
+						failed = True
+			except FileNotFoundError:
+				complain("'%s': not found" % name)
+				failed = True
+
+	else:
+		if normalize_filter(sys.stdin, sys.stdout) != OK:
+			failed = True
+	sys.exit(not failed)
