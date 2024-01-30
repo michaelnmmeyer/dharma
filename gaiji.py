29cbb61fb972344ce923ccee3d81cb54605cf435
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-01-30 12:44:44 +0100

    Update gaiji from project-documentation

--- gaiji.py
+++ gaiji.py
@@ -3,47 +3,25 @@ from dharma import config
 
 db = config.open_db("texts")
 
-def gather_images():
-	ret = {}
-	path = os.path.join(config.STATIC_DIR, "gaiji")
-	for file in os.listdir(path):
-		name, ext = os.path.splitext(file)
-		ext = ext.lower()
-		if ext not in (".svg", ".jpg", ".png"):
-			continue
-		assert not name in ret, "duplicate gaiji image %r" % file
-		url = "/gaiji/%s" % file
-		ret[name] = url
-	return ret
-
 def iter_rows():
-	path = config.path_of("gaiji.tsv")
+	path = config.path_of("repos/project-documentation/gaiji/gaiji.tsv")
 	field_names = None
 	with open(path) as f:
 		for line_no, line in enumerate(f, 1):
 			fields = [f.strip() for f in line.split("\t")]
 			if line_no == 1:
-				assert len(fields) == 4, fields
+				assert len(fields) == 3, fields
 				field_names = fields
 				continue
 			fields = dict(zip(field_names, fields))
-			assert len(fields) == 4, fields
+			assert len(fields) == 3, fields
 			yield line_no, fields
 
 def load_data():
 	ret = {}
-	imgs = gather_images()
 	for line_no, row in iter_rows():
 		assert row["names"], "the column 'names' should not be empty"
 		names = set(row["names"].split())
-		row["img"] = None
-		for name in names:
-			img = imgs.get(name)
-			if not img:
-				continue
-			assert row["img"] is None, "duplicate images for symbol %r" % name
-			row["img"] = img
-			del imgs[name]
 		if not row["description"]:
 			row["description"] = "no description available"
 		for name in names:
@@ -54,9 +32,6 @@ def load_data():
 			if not rec["text"]:
 				rec["text"] = None
 			ret[name] = rec
-	for name, img in imgs.items():
-		rec = {"name": name, "text": None, "description": None, "img": img}
-		ret[name] = rec
 	return ret
 
 def get(name):
