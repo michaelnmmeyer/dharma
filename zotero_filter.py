7dd49981983f5941cbb5adfb54f514624fd674be
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-03-07 13:47:53 +0100

    Zotero workaround

--- zotero_filter.py
+++ zotero_filter.py
@@ -9,18 +9,34 @@ def copy_response(r):
         bottle.response.set_header("Content-Type", v)
     return r.text
 
+def next_request_delay(r):
+    wait = -1
+    n = r.headers.get("Backoff", "")
+    if n.isdigit():
+        wait = max(wait, int(n))
+    n = r.headers.get("Retry-After", "")
+    if n.isdigit():
+        wait = max(wait, int(n))
+    return wait
+
+MY_API_KEY = "ojTBU4SxEQ4L0OqUhFVyImjq"
+
 @bottle.route('/groups/1633743/items')
 def reply():
     url = urlparse(bottle.request.url)
     url = url._replace(scheme="https", netloc="api.zotero.org")
     url = url.geturl()
-    print(url)
-    while True:
-        r = requests.get(url)
+    s = requests.Session()
+    s.headers["Zotero-API-Version"] = "3"
+    s.headers["Zotero-API-Key"] = MY_API_KEY
+    for i in range(5):
+        r = s.get(url)
         if r.ok:
             return copy_response(r)
-        if r.status_code != 500:
-            raise Exception
-        time.sleep(5)
+        wait = next_request_delay(r)
+        if wait < 0:
+            wait = 5
+        time.sleep(wait)
+    raise Exception(repr((r.headers, r.text)))
 
 bottle.run(host='0.0.0.0', port=8024, server='gevent', reloader=True)
