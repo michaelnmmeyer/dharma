7620c8878130253310394d7dd24cc4b1cd42c0a7
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-03-18 08:42:14 +0100

    Bug fix

--- backup_to_jawakuno.sh
+++ backup_to_jawakuno.sh
@@ -1,14 +1,16 @@
 set -e errexit
 
-test -n "$DHARMA_HOME"
+if test -z "$DHARMA_HOME"; then
+	DHARMA_HOME=./
+fi
 
-cd $DHARMA_HOME/repos/jawakuno
+cd "$DHARMA_HOME"/repos/jawakuno
 git pull
 
 inp_dir=$DHARMA_HOME/repos/tfd-nusantara-philology/output/critical-edition/texts
 out_dir=texts/txt/tfd-nusantara-philology
 
-rsync --progress --archive --delete-after $inp_dir/ $out_dir
+rsync --progress --archive --delete-after "$inp_dir/" $out_dir
 git add $out_dir
 git commit -m "Update texts from tfd-nusantara-philology" || true
 git push
