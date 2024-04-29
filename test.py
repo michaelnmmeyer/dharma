78716f8b4d2fa30377cf7460667eba0230d3365d
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-04-29 08:26:13 +0200

    Todo

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ from dharma import tree
 files = sorted(f for f in iglob("texts/DHARMA_INS*") if not "DHARMA_INSEC" in f)
 
 for f in files:
-	t = tree.parse(f)
-	for node in t.find("//*[@n and glob('*-*', @n)]"):
-		div = node.first("""ancestor::div[@type='edition' or @type='translation'
-			or @type='commentary' or @type='bibliography']""")
-		parent = div and div["type"] or "?"
-		print(parent, node.name, node["n"])
+	try:
+		t = tree.parse(f)
+	except tree.Error:
+		continue
+	for node in t.find("//revisionDesc"):
+		print(node.attrs)
 
