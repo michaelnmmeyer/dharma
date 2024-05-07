4cc00a90450108226169c48b207611eea0fe880f
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-05-07 21:15:47 +0200

    Cane inscriptions

--- manu/parse.py
+++ manu/parse.py
@@ -6,5 +6,14 @@ soup = BeautifulSoup(sys.stdin, "html.parser")
 for name in ("aside", "a"):
 	for tag in soup.find_all(name):
 		tag.decompose()
+for name in ("blockquote",):
+	for tag in soup.find_all(name):
+		tag.unwrap()
+for name in ("p", "em"):
+	for tag in soup.find_all("p"):
+		if not tag.get_text():
+			tag.decompose()
+		elif name == "em" and tag.get_text().isspace():
+			tag.unwrap()
 
 print(soup)
