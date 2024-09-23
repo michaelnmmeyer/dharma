e5bc7b9dc55bbb1eb533043a223cf5a7f91dc817
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-09-23 08:00:01 +0200

    Pursue ARIE

--- arie/parse.py
+++ arie/parse.py
@@ -48,6 +48,12 @@ des fois plusieurs. hanging indent for paragraphs in the original.
 	<supplied reason="omitted">r</supplied>
 
 H, H1, H2, H3, H4
+	differences in formatting?
+
+<H1>
+	toujours employé pour des numéros d'ARIE, on a:
+
+	<H1><arie n="088" ref="ARIE1969-1970"/>filename arie088_(1969-1970)_pp.16-77.txt</H1>
 
 <H>
 	@type is one of "subdistrict" "district" "state" "placename"
@@ -58,23 +64,14 @@ H, H1, H2, H3, H4
 	<H type="subdistrict">Ron Taluk</H>
 		petites caps et centré
 
-<Nil>
-	<pb n="109:64"/>
-</Nil>
-<Nil>
-	<pb n="110:22"/>
-</Nil>
-	enveloppe des sauts de page qui sont en-dehors de
-	<INSCRIPTION>...</INSCRIPTION>
+	difficile à extraire pour la recherche
 
 <apb>	indique que l'enregistrement se poursuit sur la page suivante;
 	le marqueur est présent dans chaque colonne qui se poursuit sur
 	la page suivante. info pas très utile, sauf pour renvoyer à des
-	numéros de page de l'édition papier.
-
-
-
-
+	numéros de page de l'édition papier. pas vérifié si des entrées
+	s'étendent sur plus de deux pages viz. s'il peut y avoir plusieurs
+	<apb> dans une seule entrée.
 
 weird:
 	<Footnote> (l'une d'elle est précédée de <sup> [1 seule occ.] pour l'appel)
