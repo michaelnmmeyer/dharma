67d8438ff9bdda8f280d644d3e3a509eb298b9f4
Michaël Meyer <michaelnm.meyer@gmail.com>
2025-02-18 19:52:53 +0100

    Pursue on SII

---
+++ sii/all.py
@@ -0,0 +1,10 @@
+"""
+Have id/@h for {volume}:{page}:{inscription_no}
+Have id/@l for {volume}:{page}:{inscription_no}:{line_no}
+Have pb/@n for {volume}:{page}
+"""
+
+from dharma import tree
+
+t = tree.parse("sii_all.xml")
+
