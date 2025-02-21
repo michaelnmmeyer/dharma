4ff116aa46633b4484fe73f521ac5afd97f777b9
Michaël Meyer <michaelnm.meyer@gmail.com>
2025-02-21 17:32:06 +0100

    Pursue on SII

--- sii/t.py
+++ sii/t.py
@@ -1,14 +1,24 @@
 from dharma import tree
 import sys, re, collections
 
-tadd = tree.parse("sii_add.hid.xml")
+#tadd = tree.parse("sii_add.hid.xml")
 tall = tree.parse("sii_all.hid.xml")
 
 tbl = {}
 for x in tall.find("//div[@type='insc']"):
-	vol, page, ins = re.fullmatch(r"([1-9][0-9]*):([1-9][0-9]*):(.+)", x["n"]).groups()
-	tbl.setdefault((vol, page), []).append(x["n"])
+	m = re.fullmatch(r"([1-9][0-9]*):([1-9][0-9]*):(.+)", x["n"])
+	assert m, x
+	vol, page, ins = m.groups()
+	tbl.setdefault((vol, ins.rstrip("X")), []).append(x)
 
-for id, items in tbl.items():
+for id, items in sorted(tbl.items()):
 	if len(items) > 1:
-		print(items)
+		Y = ""
+		for item in items:
+			item["n"] = item["n"].rstrip("X") + Y
+			Y += "Y"
+		#print(items)
+	else:
+		assert not items[0]["n"].endswith("X")
+
+sys.stdout.write(tall.xml())
