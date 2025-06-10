fa6b3a4213db0790dc6bda67ede1f5017274eec0
Michaël Meyer <michaelnm.meyer@gmail.com>
2025-06-10 14:26:49 +0200

    VG stuff

--- stuff/parse.py
+++ stuff/parse.py
@@ -136,49 +136,33 @@ def add_gaps(s):
 	ret.coalesce()
 	return ret
 
-def fix_paren(t):
-	for foreign in t.find(".//foreign"):
-		parent = foreign.parent
-		i = parent.index(foreign)
-		if i >= 1 and isinstance(parent[i - 1], tree.String) and parent[i - 1].endswith("("):
-			pass
-		else:
-			continue
-		if i < len(parent) - 1 and isinstance(parent[i + 1], tree.String) and parent[i + 1].startswith(")"):
-			pass
-		else:
-			continue
-		parent[i - 1].replace_with(parent[i - 1][:-1])
-		parent[i + 1].replace_with(parent[i + 1][1:])
-		supplied = tree.Tag("supplied", reason="explanation")
-		foreign.insert_before(supplied)
-		supplied.append(foreign)
+# # MERGE
+# def fix_unclear(s):
+# 	ret = tree.Tag("root")
+# 	i = 0
+# 	for m in re.finditer(r"\[([^[].*?)\]", s):
+# 		start, end = m.start(), m.end()
+# 		ret.append(s[i:start])
+# 		text = m.group(1)
+# 		chunks = text.split("/")
+# 		# [abc] <unclear>abc</unclear>
+# 		# [ab/cd] <choice><unclear>ab</unclear><unclear>cd</unclear></choice>
+# 		if len(chunks) == 1:
+# 			tmp = tree.Tag("unclear")
+# 			tmp.append(text)
+# 		else:
+# 			tmp = tree.Tag("choice")
+# 			for chunk in chunks:
+# 				unclear = tree.Tag("unclear")
+# 				unclear.append(chunk)
+# 				tmp.append(unclear)
+# 		ret.append(tmp)
+# 		i = end
+# 	ret.append(s[i:])
+# 	ret.coalesce()
+# 	return ret
 
-def fix_unclear(s):
-	ret = tree.Tag("root")
-	i = 0
-	for m in re.finditer(r"\[([^[].*?)\]", s):
-		start, end = m.start(), m.end()
-		ret.append(s[i:start])
-		text = m.group(1)
-		chunks = text.split("/")
-		# [abc] <unclear>abc</unclear>
-		# [ab/cd] <choice><unclear>ab</unclear><unclear>cd</unclear></choice>
-		if len(chunks) == 1:
-			tmp = tree.Tag("unclear")
-			tmp.append(text)
-		else:
-			tmp = tree.Tag("choice")
-			for chunk in chunks:
-				unclear = tree.Tag("unclear")
-				unclear.append(chunk)
-				tmp.append(unclear)
-		ret.append(tmp)
-		i = end
-	ret.append(s[i:])
-	ret.coalesce()
-	return ret
-
+# MERGE
 def fix_supplied(s):
 	ret = tree.Tag("root")
 	i = 0
@@ -192,6 +176,66 @@ def fix_supplied(s):
 	ret.append(s[i:])
 	ret.coalesce()
 	return ret
+
+def replace_delimited_in(root, start, end, tag):
+	assert isinstance(root, tree.Branch)
+	nodes = list(root)
+	x = 0
+	while x < len(nodes):
+		node = nodes[x]
+		if not isinstance(node, tree.String):
+			x += 1
+			continue
+		i = str(node).find(start)
+		if i < 0:
+			x += 1
+			continue
+		y = x
+		j = str(node).find(end, i + len(start))
+		if j < 0:
+			y += 1
+			while y < len(nodes):
+				node = nodes[y]
+				if not isinstance(node, tree.String):
+					y += 1
+					continue
+				j = str(node).find(end)
+				if j >= 0:
+					break
+				y += 1
+		if j < 0:
+			x += 1
+			continue
+		if x == y:
+			node = nodes[x]
+			tmp = tree.Tag("tmp")
+			tmp.append(node[:i])
+			repl = tag.copy()
+			repl.append(node[i + len(start):j])
+			tmp.append(repl)
+			tmp.append(node[j + len(end):])
+			nodes[x] = tmp[-1]
+			node.replace_with(tmp)
+			tmp.unwrap()
+			continue
+		repl = tag.copy()
+		repl.append(nodes[x][i + len(start):])
+		for node in nodes[x + 1:y]:
+			repl.append(node)
+		repl.append(nodes[y][:j])
+		nodes[x].replace_with(nodes[x][:i])
+		tmp = tree.String(nodes[y][j + len(end):])
+		nodes[y].replace_with(tmp)
+		nodes[y] = tmp
+		nodes[x].insert_after(repl)
+		x = y
+
+def replace_delimited(root, start, end, tag):
+	replace_delimited_in(root, start, end, tag)
+	for node in root:
+		if not isinstance(node, tree.Tag):
+			continue
+		replace_delimited(node, start, end, tag)
 
 def process_ins(ins):
 	out = tree.parse("tpl.xml")
@@ -210,10 +254,10 @@ def process_ins(ins):
 	for gr in tmp.find(".//foreign"):
 		gr.name = "hi"
 		gr["rend"] = "grantha"
-	for s in strings(tmp):
-		r = fix_unclear(s.data)
-		s.replace_with(r)
-		r.unwrap()
+	# for s in strings(tmp):
+	# 	r = fix_unclear(s.data)
+	# 	s.replace_with(r)
+	# 	r.unwrap()
 	tmp.coalesce()
 	for s in strings(tmp):
 		s.replace_with(fix_vowels(s.data))
@@ -225,14 +269,13 @@ def process_ins(ins):
 	# translation
 	tmp = out.first("//div[@type='translation']")
 	tmp.extend(ins.translation)
-	fix_paren(tmp)
 	tmp.coalesce()
 	# all
 	out.coalesce()
-	for s in strings(out):
-		r = fix_supplied(s.data)
-		s.replace_with(r)
-		r.unwrap()
+	# for s in strings(out):
+	# 	r = fix_supplied(s.data)
+	# 	s.replace_with(r)
+	# 	r.unwrap()
 	out = out.xml()
 	out = re.sub(r"(…|\.\.\.)(…|\.)*", ' <gap reason="lost" extent="unknown" unit="character"/> ', out)
 	out = re.sub(r" +", " ", out)
@@ -247,6 +290,10 @@ dans la trad:
 	[abc] supplied reason=subaudible
 	(abc) supplied reason=explanation
 
+dans l'édition:
+	{{abc}} supplied reason=lost
+	[ab/cd] <choice><unclear>ab</unclear><unclear>cd</unclear></choice>
+	[abc] <unclear>abc</unclear>
 """
 
 def mkident(ins):
