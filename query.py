778d0aef64875d9e2c6b2d4a4073b2ab89ce774d
Michaël Meyer <michaelnm.meyer@gmail.com>
2025-09-11 18:59:20 +0200

    Cursors interface, overlap

--- query.py
+++ query.py
@@ -53,18 +53,17 @@ class Tree(Cursor):
 	def next(self):
 		if self.start == -2:
 			return False
-		self.cursor.next()
-		match_start, match_len = self.cursor.match()
-		if match_start < 0:
-			self.start = -2
-			self.length = 0
-			return False
-		self.transformer.seek(match_start)
-		assert self.transformer.search_offset == match_start
-		self.start = self.transformer.display_offset
-		self.transformer.seek(match_start + match_len)
-		assert self.transformer.search_offset == match_start + match_len
-		self.length = self.transformer.display_offset - self.start
+		while True:
+			self.cursor.next()
+			match_start, match_len = self.cursor.match()
+			if match_start < 0:
+				self.start = -2
+				self.length = 0
+				return False
+			start, length = self.transformer.translate(match_start, match_len)
+			if start >= 0:
+				break
+		self.start, self.length = start, length
 		return True
 
 	def match(self):
@@ -79,7 +78,7 @@ class Tokenizer(Cursor):
 		self.search_offset = 0
 		self.display_offset = 0
 
-	def next(self):
+	def try_next_chars(self):
 		assert self.chunk <= len(self.chunks)
 		if self.chunk >= len(self.chunks):
 			return ""
@@ -91,28 +90,68 @@ class Tokenizer(Cursor):
 			if self.chunk >= len(self.chunks):
 				return ""
 			chunk = self.chunks[self.chunk]
-		cs = chunk[self.chunk_offset].casefold()
+		chars = chunk[self.chunk_offset].casefold()
 		self.chunk_offset += 1
-		self.display_offset += 1
-		self.search_offset += len(cs)
-		return cs
+		return chars
 
-	def seek(self, offset):
-		assert self.search_offset <= offset
-		while self.search_offset < offset:
-			self.next()
-		assert self.search_offset == offset
+	def next_chars(self):
+		ret = self.try_next_chars()
+		if not ret:
+			raise Exception
+		return ret
 
-	def transform(self):
-		buf = []
-		while (cs := self.next()):
-			buf.append(cs)
-		return "".join(buf)
+	def translate(self, start, length):
+		# süße > süsse
+		# search "se": süs[se] ; arrondir en-dessous sü[sse] (2, 4)
+		# search "üs": s[üs]se ; arrondir au-dessus  s[üss]e (1, 3)
+		dstart = -1
+		dlength = 0
+		assert start >= 0 and length > 0
+		if self.search_offset > start:
+			if self.search_offset >= start + length:
+				# We are in this situation if there are more
+				# than one match in a single display unit. For
+				# instance, if the original text is "süße", the
+				# search text is "süsse", and the query is "s",
+				# there will be two matches for the single
+				# display unit "ß". In this case, we will
+				# highlight "ß" a single time, so we ignore the
+				# second match.
+				return -1, 0
+			start = self.search_offset
+		assert self.search_offset <= start
+		while self.search_offset < start:
+			cs = self.next_chars()
+			if self.search_offset + len(cs) > start:
+				dstart = self.display_offset
+				self.display_offset += 1
+				self.search_offset += len(cs)
+				if self.search_offset >= start + length:
+					dlength = self.display_offset - dstart
+					return dstart, dlength
+			else:
+				self.display_offset += 1
+				self.search_offset += len(cs)
+		if dstart < 0:
+			dstart = self.display_offset
+		assert self.search_offset >= start
+		while self.search_offset < start + length:
+			cs = self.next_chars()
+			if self.search_offset + len(cs) > start + length:
+				self.display_offset += 1
+				dlength = self.display_offset - dstart
+				self.search_offset += len(cs)
+				return dstart, dlength
+			self.display_offset += 1
+			self.search_offset += len(cs)
+		dlength = self.display_offset - dstart
+		return dstart, dlength
 
 if __name__ == "__main__":
-	substring = Substring("ss", "süsse")
+	substring = Substring("se", "süsse")
 	t = tree.Tree()
 	t.append("süße")
 	cursor = Tree(t, substring)
 	while cursor.next():
-		print(*cursor.match())
+		start, length = cursor.match()
+		print(start, start + length)
